e9b672b665f52a1cf877af982de03b5c3ce2debc
tics-design <tics@elbuengusto.com.co>
2026-09-13 11:38:28 -0500

    feat: crear clase principal Spring Boot

---
+++ src/main/java/com/ebg/firma/SistemaFirmaElectronicaApplication.java
@@ -0,0 +1,12 @@
+package com.ebg.firma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFirmaElectronicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaFirmaElectronicaApplication.class, args);
+    }
+}
